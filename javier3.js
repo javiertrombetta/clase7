b9182e384ce402f51838e70df8d728917a45db7e
galvancamilanicole <galvancamilanicole@gmail.com>
2021-08-04 17:11:36 -0300

    Modifique javier3

--- javier3.js
+++ javier3.js
@@ -2,3 +2,6 @@ const nombres = require('./javier1.js');
 const apellidos = require('./javier2.js');
 
 console.log ("Hola, soy",...nombres,...apellidos);
+
+console.log("Hola, soy Camila");
+console.log("Buenas tardes");
